0a01e9978c56fac41a032bb95a89da519f1617db
Ross Badgett <rbadgett@saintmichael.org>
2015-07-15 16:46:24 +0000

    Checkpoint 22 done - Menu

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,17 +10,18 @@ class AddressBook
   
      
    def add_entry(name, phone, email)
- # #9
      index = 0
      @entries.each do |entry|
- # #10
        if name < entry.name
          break
        end
        index += 1
      end
- # #11
      @entries.insert(index, Entry.new(name, phone, email))
+   end
+  
+  def remove_entry(entry)
+     @entries.delete(entry)
    end
  
  end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,7 +18,7 @@
      end
     end
    
-      context "#add_entry" do
+    context "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -34,6 +34,16 @@
        expect(new_entry.name).to eq 'Ada Lovelace'
        expect(new_entry.phone_number).to eq '010.012.1815'
        expect(new_entry.email).to eq 'augusta.king@lovelace.com'
+     end
+   end
+
+    context "#remove_entry" do
+     it "removes one entry from the address book" do
+       book = AddressBook.new
+       entry = book.entries[0]
+       book.remove_entry(entry)
+ 
+       expect(book.entries.size).to eq 0
      end
    end
  end
